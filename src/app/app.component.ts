bec199c79fd32bb7ef13bd9dfb7799d6f84b9fed
PoHeng <alchemist609@gmail.com>
2023-03-26 15:29:03 +0800

    調整footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { style } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./style.css'],
+  encapsulation: ViewEncapsulation.None,
   styles: [
   ],
 })
